118ae2ab71c3ce4185de7c6bd5c5c7d74d326a45
Тимофеев Денис <denis@alc.com>
2021-07-26 15:44:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class TestApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, I am Android app")
+
+
+TestApp().run()
